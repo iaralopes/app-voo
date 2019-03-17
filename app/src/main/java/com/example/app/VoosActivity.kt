608dc027f77bed3edcc99802c5b70d6d1f212828
Iara Lopes <iarasiria@gmail.com>
2019-03-16 22:52:47 -0300

    Estruturando a tela inicial

--- app/src/main/java/com/example/app/VoosActivity.kt
+++ app/src/main/java/com/example/app/VoosActivity.kt
@@ -3,10 +3,10 @@ package com.example.app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class VoosActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_voos)
     }
 }
